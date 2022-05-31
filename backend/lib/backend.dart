c8c7487ef50e78ac905a9872a6b44a678d24ec55
luowensheng <luowensheng2018@gmail.com>
2022-06-01 00:51:32 +0800

    added readme

--- backend/lib/backend.dart
+++ backend/lib/backend.dart
@@ -5,22 +5,30 @@ import 'package:backend/server/server.dart';
 void run() {
   var app = Server();
   app.get("/home", (responseWriter, request) {
-    responseWriter.setContent("home");
+    responseWriter.setContent("""
+       <h1>Hello Surla</h1>
+       <script>
+       document.querySelector("h1").addEventListener("click", function(){
+            this.textContent = new Date();
+       })
+       </script>
+
+          """);
+    responseWriter["body"] = "Home";
   });
 
-    app.get("/about", (responseWriter, request) {
+  app.get("/about", (responseWriter, request) {
     responseWriter.setContent("about");
   });
 
-    app.get("/surla", (responseWriter, request) {
+  app.get("/surla", (responseWriter, request) {
     responseWriter.setContent("surla");
   });
 
-    app.get("/api", (responseWriter, _) {
+  app.get("/api", (responseWriter, _) {
     responseWriter.setJsonContent(jsonEncode({"success": true}));
   });
 
-  
   app.listen(() {
     print("server listening at http://localhost:${app.port}");
   });
--- backend/lib/server/server.dart
+++ backend/lib/server/server.dart
@@ -9,7 +9,6 @@ import 'package:backend/server/response_writer.dart';
 class Server {
   late HashMap<String, HashMap<String, Function(ResponseWriter, Request)>>
       urlMapping;
-  late ServerSocket server;
   late int port;
   Server({int port = 8000}) {
     this.port = port;
@@ -22,13 +21,9 @@ class Server {
     urlMapping["DELETE"] = HashMap<String, Function(ResponseWriter, Request)>();
   }
 
-  setup() async {
-    this.server = await ServerSocket.bind(InternetAddress.anyIPv4, port);
-  }
-
-  void listen(Function function) async {
-    await setup();
-    function();
+  void listen(Function onMount) async {
+    var server = await ServerSocket.bind(InternetAddress.anyIPv4, port);
+    onMount();
     server.listen((client) {
       handleConnection(client);
     },
@@ -51,12 +46,43 @@ class Server {
     client.close();
   }
 
-  void get(String pattern, Function(ResponseWriter, Request) function) {
-    route("GET", pattern, function);
+  void handleFavicon(ResponseWriter rw, Request r) {
+    // https://img.icons8.com/ios-filled/344/internet.png
+    const fav = """
+    <html>
+    <head>
+    </head>
+    <body>
+    <img src="https://img.icons8.com/ios-filled/344/internet.png">
+    </body>
+    </html>
+    """;
+    rw.setContent(fav);
+    // rw.setContentType("image/*");
+
+    // rw["accept"] = "image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8";
+    // rw["accept-encoding"] = "gzip, deflate, br";
+    // rw["accept-language"] = "en-US,en;q=0.9";
+    // rw["referer"] = "https://img.icons8.com/ios-filled/344/internet.png";
+    // rw["sec-ch-ua"] =
+    //     """" Not A;Brand";v="99", "Chromium";v="101", "Microsoft Edge";v="101""";
+    // rw["sec-ch-ua-mobile"] = "?0";
+    // rw["sec-ch-ua-platform"] = "Windows";
+    // rw["sec-fetch-dest"] = "image";
+    // rw["sec-fetch-mode"] = "'no-cors'";
+    // rw["sec-fetch-site"] = "same-origin";
+    // rw["user-agent"] =
+    //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53";
   }
 
   void processRequest(ResponseWriter rw, Request r) {
+    if (r.partialUrl.contains("favicon.i")) return handleFavicon(rw, r);
+
     urlMapping[r.method]![r.partialUrl]!(rw, r);
+  }
+
+  void get(String pattern, Function(ResponseWriter, Request) function) {
+    route("GET", pattern, function);
   }
 
   void post(String pattern, Function(ResponseWriter, Request) function) {
